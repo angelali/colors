57adb761b5c936b575d07627cf042c9fc5f7fd47
Angela Li <angelaconstanceli@gmail.com>
2014-04-28 23:42:44 -0400

    done with i/o

--- colors_arduino/colors_arduino.ino
+++ colors_arduino/colors_arduino.ino
@@ -21,29 +21,34 @@ int tempBoard[4][4];
 boolean gameInProgress = false;
 boolean buttonPressed = false;
 
-// Pins for inputs (push buttons) and outputs (shift registers / LEDs)
+// Pins for inputs (push buttons)
 const int UP_PIN = A0;
-const int RIGHT_PIN = 0; // TODO
-const int DOWN_PIN = 0; // TODO
-const int LEFT_PIN = 0; // TODO
-const int FOO = 13; // TODO
+const int RIGHT_PIN = A1;
+const int DOWN_PIN = A2;
+const int LEFT_PIN = A3;
 
-const int colorMap[8] = {0, 0, 0, 0, 0, 0, 0, 0}; // TODO
-const int pinMap[4]]4]; // TODO
+// Pins for outputs (shift registers / LEDs)
+const int LATCH_PIN = 8; // green
+const int CLOCK_PIN = 12; // yellow
+const int DATA_PIN = 11; // blue
 
 /* Initializes all connections and creates a new game. */
 void setup() {
     // Initialize serial
     Serial.begin(9600);
 
-    // Initialize inputs and outputs
+    // Initialize inputs (push buttons) and outputs (shift registers and LEDs)
     pinMode(UP_PIN, INPUT);
     pinMode(RIGHT_PIN, INPUT);
     pinMode(DOWN_PIN, INPUT);
     pinMode(LEFT_PIN, INPUT);
-    pinMode(FOO, OUTPUT); // TODO
 
-    // TODO ~ Seed the PRNG with something reasonable
+    pinMode(LATCH_PIN, OUTPUT);
+    pinMode(CLOCK_PIN, OUTPUT);
+    pinMode(DATA_PIN, OUTPUT);
+
+    // Seed the PRNG with the read from an unconnected pin
+    randomSeed(analogRead(A4));
 
     // Begin game
     newGame();
@@ -57,27 +62,27 @@ void loop() {
     }
 
     // Read inputs
-    int upVal = digitalRead(UP_PIN);
-    int rightVal = digitalRead(RIGHT_PIN);
-    int downVal = digitalRead(DOWN_PIN);
-    int leftVal = digitalRead(LEFT_PIN);
+    int upVal = analogRead(UP_PIN);
+    int rightVal = analogRead(RIGHT_PIN);
+    int downVal = analogRead(DOWN_PIN);
+    int leftVal = analogRead(LEFT_PIN);
 
     // Confirm that there was no previous button press (so we don't respond too many times)
     if (!buttonPressed) {
         // Make a move
-        if (upVal == HIGH) {
+        if (upVal) {
             buttonPressed = true;
             makeMove(UP);
         }
-        else if (rightVal == HIGH) {
+        else if (rightVal) {
             buttonPressed = true;
             makeMove(RIGHT);
         }
-        else if (downVal == HIGH) {
+        else if (downVal) {
             buttonPressed = true;
             makeMove(DOWN);
         }
-        else if (leftVal == HIGH) {
+        else if (leftVal) {
             buttonPressed = true;
             makeMove(LEFT);
         }
@@ -145,24 +150,44 @@ boolean attemptMove(int dir) {
 
     switch(dir) {
         case UP:
-            // TODO
+            for (int j = 0; j <= 3; j++) {
+                for (int i = 0; i <= 3; i++) {
+                    if (i == 0) {
+                        continue;
+                    }
+                    movedAnythingYet = attemptTileMove(i, j, i - 1, j) || movedAnythingYet;
+                }
+            }
         break;
 
         case RIGHT:
-            // TODO
+            for (int i = 0; i <= 3; i++) {
+                for (int j = 3; j >= 0; j--) {
+                    if (j == 3) {
+                        continue;
+                    }
+                    movedAnythingYet = attemptTileMove(i, j, i, j + 1) || movedAnythingYet;
+                }
+            }
         break;
 
         case DOWN:
-            // TODO
+            for (int j = 0; j <= 3; j++) {
+                for (int i = 3; i >= 0; i--) {
+                    if (i == 3) {
+                        continue;
+                    }
+                    movedAnythingYet = attemptTileMove(i, j, i + 1, j) || movedAnythingYet;
+                }
+            }
         break;
 
         case LEFT:
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
+            for (int i = 0; i <= 3; i++) {
+                for (int j = 0; j <= 3; j++) {
                     if (j == 0) {
                         continue;
                     }
-
                     movedAnythingYet = attemptTileMove(i, j, i, j - 1) || movedAnythingYet;
                 }
             }
@@ -174,8 +199,7 @@ boolean attemptMove(int dir) {
         return false;
     }
 
-    // Otherwise, it was valid, so add a new tile
-    // TODO
+    // TODO ~ Insert new tile
 
     return true;
 }
@@ -189,33 +213,67 @@ void checkLost() {
     }
 
     // Whoops, we lost the game
-    lostGame();
-}
-
-void lostGame() {
-    // Update game state
     gameInProgress = false;
-
-    // TODO ~ Do something to show they lost (womp womp)
-}
-
-void wonGame() {
-    // Update game state
-    gameInProgress = false;
-
-    // TODO ~ Do something to show they won (woohooo!)
+    Serial.println("L");
 }
 
 /****************************************
  ********** DISPLAY  METHODS ************
  ****************************************/
 
- void renderBoard() {
-    // TODO ~ Hahahahahahahaha
+byte colors[8] = {7, 6, 4, 5, 1, 3, 2, 0};
+
+byte color(int tileNum) {
+    tileNum -= 1;
+    int i = tileNum / 4;
+    int j = tileNum % 4;
+    int value = board[i][j];
+    return colors[value];
+}
+
+void renderBoard() {
+    byte A = color(15) % 2     << 0
+           + color( 1)         << 1
+           + color( 2)         << 4
+           + color( 3) % 2     << 7;
+
+    byte B = color(15) % 4 / 2 << 0
+           + color( 3) / 2     << 1
+           + color( 4)         << 3
+           + color( 5) % 4     << 6;
+
+    byte C = color(15) / 4     << 0
+           + color( 5) / 4     << 1
+           + color( 6)         << 2
+           + color( 7)         << 5;
+
+    byte D = color(16) % 2     << 0
+           + color( 8)         << 1
+           + color( 9)         << 4
+           + color(10) % 2     << 7;
+
+    byte E = color(16) % 4 / 2 << 0
+           + color(10) / 2     << 1
+           + color(11)         << 3
+           + color(12) % 4     << 6;
+
+    byte F = color(16) / 4     << 0
+           + color(12) / 4     << 1
+           + color(13)         << 2
+           + color(14)         << 5;
+
+    digitalWrite(LATCH_PIN, LOW);
+    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, F);
+    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, E);
+    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, D);
+    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, C);
+    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, B);
+    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, A);
+    digitalWrite(LATCH_PIN, HIGH);
  }
 
- /* Prints board to serial. For debugging. */
- void printBoard() {
+/* Prints board to serial. For debugging. */
+void printBoard() {
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             Serial.print(board[i][j]);
@@ -224,13 +282,13 @@ void wonGame() {
     }
  }
 
- /****************************************
- ********** UTILITY METHODS *************
- ****************************************/
+/****************************************
+********** UTILITY METHODS *************
+****************************************/
 
- /* Randomly returns either 0 or 1. */
+/* Randomly returns either 1 or 2. */
 int randomTile() {
-    return random(0, 2);
+    return random(1, 3);
 
 }
 
@@ -258,6 +316,13 @@ boolean attemptTileMove(int i, int j, int i2, int j2) {
     if (tempBoard[i][j] == tempBoard[i2][j2]) {
         tempBoard[i][j] = 0;
         tempBoard[i2][j2]++;
+
+        // They won!
+        if (tempBoard[i2][j2] == 6) {
+            gameInProgress = false;
+            Serial.println("W");
+        }
+
         return true;
     }
 
